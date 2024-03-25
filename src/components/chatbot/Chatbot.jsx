461b9e0ca28e8ce101c269edaaf2523cf8f20c51
Kane Xu <xu.hong@northeastern.edu>
2024-03-25 10:59:49 -0400

    dev push 3.0 beta

--- src/components/chatbot/Chatbot.jsx
+++ src/components/chatbot/Chatbot.jsx
@@ -25,7 +25,7 @@ export default function Home() {
   }
 
   const handlePrompt = (promptText) => {
-    const msg: Message = { id: crypto.randomUUID(),  content: promptText, role: 'user' };
+    const msg = { id: crypto.randomUUID(),  content: promptText, role: 'user' };
     append(msg, { options: { body: { useRag, llm, similarityMetric}}});
     console.log(msg)
   };
